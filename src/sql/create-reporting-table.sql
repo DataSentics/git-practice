103bb9654f605163dda181c9487a4ded70063d86
pkolarovec@csas.cz <pkolarovec@csas.cz>
2022-08-30 13:06:08 +0000

    Fixed wrong table name

--- src/sql/create-reporting-table.sql
+++ src/sql/create-reporting-table.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS reporting_table(id INT, amount INT, month STRING)
 -- COMMAND ----------
 
 -- This does not want to work for me :(  maybe I am using wrong characters in the table. Do not know but need to leave for vacation. See you later aligator. 
-INSERT INTO reporting-table VALUES
+INSERT INTO reporting_table VALUES
 (1, 1000, "AUG"),
 (2, 2000, "SEP"),
 (3, 3000, "OCT"),
@@ -25,3 +25,7 @@ INSERT INTO reporting-table VALUES
 -- COMMAND ----------
 
 SELECT * FROM reporting_table
+
+-- COMMAND ----------
+
+
